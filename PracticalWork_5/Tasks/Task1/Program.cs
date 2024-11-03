a06dc0677e57d5c83e4b4920fae03e812e85b1c8
Кирилл Талибаев <163425382+Kirif-T@users.noreply.github.com>
2024-11-03 21:14:06 +0400

    Add files via upload

---
+++ PracticalWork_5/Tasks/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string input = Console.ReadLine();
+
+            // Разделяем предложение на слова
+            string[] words = SplitText(input);
+
+            // Вывод каждого слова на отдельную строку
+            PrintWords(words);
+        }
+
+        // Метод разделения текста на слова
+        static string[] SplitText(string text)
+        {
+            return text.Split(' '); // Разделение строки по пробелам
+        }
+
+        // Метод вывода каждого слова на отдельной строке
+        static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ PracticalWork_5/Tasks/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputPhrase = Console.ReadLine();
+            string result = ReverseWords(inputPhrase);
+            Console.WriteLine("Результат: " + result);
+        }
+
+        static string ReverseWords(string inputPhrase)
+        {
+            string[] words = SplitText(inputPhrase);
+            Array.Reverse(words);
+
+            // Преобразуем первую букву первого слова и последнего слова
+            if (words.Length > 0)
+            {
+                words[0] = char.ToUpper(words[0][0]) + words[0].Substring(1).ToLower(); // Первое слово
+            }
+            if (words.Length > 1)
+            {
+                words[words.Length - 1] = char.ToLower(words[words.Length - 1][0]) + words[words.Length - 1].Substring(1); // Последнее слово
+            }
+
+            string resultString = string.Empty;
+
+            // Собираем обратно в строку
+            foreach (string word in words)
+            {
+                resultString += word + " ";
+            }
+
+            return resultString.Trim(); // Удаляем последний пробел
+        }
+
+        static string[] SplitText(string text)
+        {
+            return text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
